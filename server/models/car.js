24902d36f95772df16ce5d5bd040c9c2d49471d4
Sebastian <sebyra@gmail.com>
2018-11-13 19:22:10 -0300

    agregado el numero de puertas

--- server/models/car.js
+++ server/models/car.js
@@ -5,7 +5,8 @@ const carSchema = new Schema({
   brand: { type: String, required: true },
   model: { type: String, required: true },
   category: { type: String, required: true },
-  price: { type: Number, required: true }
+  price: { type: Number, required: true },
+  numDoors: { type: Number, required: true}
 });
 
 module.exports = mongoose.model("Car", carSchema);
