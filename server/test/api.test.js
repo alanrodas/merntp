f52c0d0cb40a4daa2f0147013678f7f0b2d7bdd0
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-11-13 19:00:29 -0300

    test post cars

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -3,7 +3,6 @@ var assert = require('assert');
 Axios.defaults.baseURL = 'http://localhost:3001';
 
 describe("API cars add y remove", () => {
-
     describe("Success", () => {
 
         it("POST car", async () => {
@@ -18,22 +17,61 @@ describe("API cars add y remove", () => {
             assert.equal(r.status, 201);
         });
 
+        it("POST car - with out price", async () => {
+            let car = {
+                brand: "Renault",
+                model: "Clio2",
+                category: "A",
+                numDoors: 3
+            }
+            let r = await Axios.post("/api/cars/", car)
+            assert.equal(r.status, 201);
+        });
     })
 
-    // describe("Fail", () => {
+    describe("Fail", () => {
 
-    //     it("POST car - without brand", async done => {
-    //         let car = {
-    //             model: "Clio2",
-    //             category: "A",
-    //             price: 1000000,
-    //             numDoors: 3
-    //         }
-    //         let r = await Axios.post("/api/cars/", car)
-    //         assert.equals(r.status, 500);
-    //         done();
-    //     });
-    // })
+        describe("POST car", () => {
+            it("without brand", async () => {
+                let car = {
+                    model: "Clio2",
+                    category: "A",
+                    price: 1000000,
+                    numDoors: 3
+                }
+                try {
+                    await Axios.post("/api/cars/", car)
+                } catch (error) {
+                    assert.equal(error.response.status, 500);
+                }
+            })
 
-
+            it("without model", async () => {
+                let car = {
+                    brand: "Mercedez-Benz",
+                    category: "A",
+                    price: 1000000,
+                    numDoors: 3
+                }
+                try {
+                    await Axios.post("/api/cars/", car)
+                } catch (error) {
+                    assert.equal(error.response.status, 500);
+                }
+            });
+            it("without numDoors", async () => {
+                let car = {
+                    brand: "Mercedez-Benz",
+                    category: "A",
+                    model: "Cuchuflin",
+                    price: 1000000
+                }
+                try {
+                    await Axios.post("/api/cars/", car)
+                } catch (error) {
+                    assert.equal(error.response.status, 500);
+                }
+            });
+        })
+    })
 })
