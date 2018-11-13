f8cbdead02ac57ee1817aabd6e9f52da2a294db5
Cristina Raimondo <ochocaracteres.raimondo1@gmail.com>
2018-11-13 18:38:56 -0300

    modique el server

--- server/controllers/car.controller.js
+++ server/controllers/car.controller.js
@@ -1,7 +1,9 @@
-const Car = require("../models/car");
+const Car = require('../models/car');
 const carCtrl = {};
 
 carCtrl.getCars = async (req, res, next) => {
+  const cars = await Car.find();
+  res.ok(cars);
   //to be implemented
   // res.ok(data); or  res.internalServerError();
 };
@@ -12,7 +14,8 @@ carCtrl.createCar = async (req, res, next) => {
       brand: req.body.brand,
       model: req.body.model,
       category: req.body.category,
-      price: req.body.price
+      price: req.body.price,
+      numDoors: req.body.numDoors
     });
     await car.save();
     res.created(car);
@@ -36,11 +39,32 @@ carCtrl.getCar = async (req, res, next) => {
 };
 
 carCtrl.editCar = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const car = await Car.findById(id);
+    if (car) {
+      const { brand, model, category, price, numDoors } = req.body;
+      const newCar = { brand, model, category, price, numDoors };
+      await Car.findByIdAndUpdate(req.params.id, newCar);
+
+      res.noContent();
+    } else {
+      res.notFound();
+    }
+  } catch (exception) {
+    res.internalServerError();
+  }
   // to be implemented
   // res.noContent(); or  res.internalServerError(); or  res.notFound();
 };
 
 carCtrl.deleteCar = async (req, res, next) => {
+  try {
+    await Car.findByIdAndRemove(req.params.id);
+    res.json({ Status: 'car deleted' });
+  } catch (exception) {
+    res.internalServerError();
+  }
   // to be implemented
   // res.noContent(); or  res.internalServerError(); or  res.notFound();
 };
