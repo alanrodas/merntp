a3b590aeef19428f31306bab05e653fb9b0a9948
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-11-14 03:50:40 -0300

    inicio ordenar lista de autos

--- client/src/components/CarBrowse/CarBrowse.jsx
+++ client/src/components/CarBrowse/CarBrowse.jsx
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import { Table } from "reactstrap";
 import "./CarBrowse.css";
 import ListCars from "./ListCars/ListCars";
+import OrderButton from "./OrderButton";
 import Context from "../../Context";
 
 import axios from "axios";
 
 class CarBrowse extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: ""
+    };
+  }
+
   componentDidMount() {
     this.requestCars();
   }
@@ -20,15 +28,37 @@ class CarBrowse extends Component {
       .catch(e => console.log("ERROR", e));
   }
 
+  select(i) {
+    this.setState({ selected: i });
+  }
+
   render() {
     return (
       <Table dark>
         <thead>
           <tr>
-            <th scope="col"> Brand </th>
+            <th scope="col">
+              <OrderButton
+                name="brand"
+                select={() => this.select("brand")}
+                selected={this.state.selected}
+              />
+            </th>
             <th scope="col"> Model </th>
-            <th scope="col"> Category </th>
-            <th scope="col"> Price </th>
+            <th scope="col">
+              <OrderButton
+                name="category"
+                select={() => this.select("category")}
+                selected={this.state.selected}
+              />
+            </th>
+            <th scope="col">
+              <OrderButton
+                name="price"
+                select={() => this.select("price")}
+                selected={this.state.selected}
+              />
+            </th>
             <th scope="col"> Actions </th>
           </tr>
         </thead>
---
+++ client/src/components/CarBrowse/OrderButton.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+
+class OrderButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      graphicUp: false
+    };
+  }
+
+  changeGraphic() {
+    this.setState({
+      graphicUp: !this.state.graphicUp
+    });
+  }
+
+  nameCapitalized() {
+    let name = this.props.name;
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
+  render() {
+    if (this.props.selected === this.props.name) {
+      return (
+        <button onClick={() => this.changeGraphic()} className="btn btn-link">
+          {this.nameCapitalized()}
+          {this.state.graphicUp ? <FaAngleUp /> : <FaAngleDown />}
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.props.select} className="btn btn-link">
+          {this.nameCapitalized()}
+        </button>
+      );
+    }
+  }
+}
+
+export default OrderButton;
